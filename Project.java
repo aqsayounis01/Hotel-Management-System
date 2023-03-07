dd53b7d7272e5492f1500e1b7d9e1ea6d51b0812
Aqsa <aqsayounis2004@gmail.com>
2023-03-07 18:38:59 +0530

    modified customer

--- Project.java
+++ Project.java
@@ -6,13 +6,34 @@ class HotelMS {
         Scanner s=new Scanner(System.in);
 
         int x;
-        x= s.nextInt();
+
         System.out.println("Welcome to Hotel Management System!\nWe are always here to help you :)");
         System.out.println("Press 1 for customer services\nPress 0 for staff services");
+        x= s.nextInt();
         switch(x)
         {
             case 1:
                 System.out.println("Dear customer, How can we help you?");
+                System.out.println("Enter F for food related services \nT for Transport related services\nC for complaints:");
+                char ch;
+                ch= s.next().charAt(0);
+                switch(ch)
+                {
+                    case 'F':
+                        System.out.println("food related services ");
+                        break;
+                    case 'T':
+                        System.out.println("transport related services ");
+                        break;
+                    case 'C':
+                        System.out.println("submit your complain");
+                        break;
+                        default:
+                            System.out.println("Invalid Character");
+
+
+
+                }
                 break;
         }
     }
