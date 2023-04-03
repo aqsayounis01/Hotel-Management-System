4e9f1ee277c58c6453f1062342f2b72daaee8070
Aqsa <aqsayounis2004@gmail.com>
2023-04-04 02:17:08 +0530

    without files

--- Project.java
+++ Project.java
@@ -1,15 +1,16 @@
 package HMS;
 
 import java.util.Scanner;
+
 class HotelMS {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
 
-        int x;
-        int []av= new int[50];
+        int x,n=50;
+        int []av= new int[n];
         //room no's
-        for (int i = 0; i < 50 ; i++) {
-            av[i]=i+1;
+        for (int i = 0; i < n ; i++) {
+            av[i]=i;
         }
         System.out.println("Welcome to Hotel Management System!\nWe are always here to help you :)");
         System.out.println("Press 1 for customer services\nPress 0 for staff services");
@@ -21,40 +22,16 @@ class HotelMS {
                 System.out.println("Enter B for Hotel Booking \nC for check out\nR for requests or complains:");
                 char ch;
                 ch= s.next().charAt(0);
-
+                int rn;
                 switch(ch)
                 {
                     case 'B':
-                        int rn;
+
                         String name;
 
                         String place;
                         int people;
                         System.out.println("Hotel Booking:");
-                        System.out.println("enter which room no you want to book (1-50):");
-                        rn=s.nextInt();
-
-
-                        System.out.println("enter your name:");
-                        s.nextLine();
-                        name=s.nextLine();
-
-
-                        System.out.println(name);
-                        System.out.println("enter your place:");
-
-
-                        place=s.nextLine();
-                        System.out.println(place);
-
-
-                        System.out.println("enter no of person:");
-people=s.nextInt();
-
-                        System.out.println(people);
-
-
-
                         // enter details in the file with room no
 
                         int ac=2500,pay,corr;
@@ -77,16 +54,47 @@ people=s.nextInt();
                             System.out.println("invalid amount, Transaction unsuccessful...");
                         }
 
+                        System.out.println("enter which room no you want to book (1-49):");
 
+                        rn=s.nextInt();
+
+                        for (int i = rn; i < n-1; i++) {
+                            av[i]=av[i+1];
+                        }
+                        n--;
+                        System.out.println("enter your name:");
+                        s.nextLine();
+                        name=s.nextLine();
+
+
+                        System.out.println(name);
+                        System.out.println("enter your place:");
+
+
+                        place=s.nextLine();
+                        System.out.println(place);
+
+
+                        System.out.println("enter no of person:");
+                        people=s.nextInt();
+
+                        System.out.println(people);
 
                         break;
                     case 'R':
                         System.out.println("Requests/Complains");
                         //user will enter the string mentioning the room no and that will be copied into the file
+                        String comp;
+                        comp=s.nextLine();
+
                         break;
                     case 'C':
                         System.out.println("Check out");
-                        // just realese the room in the array
+                        // just release the room in the array
+                        n++;
+                        for (int i = 0; i <50 ; i++) {
+                            av[i]=i;
+                        }
                         break;
                         default:
                             System.out.println("Invalid Character");
@@ -109,6 +117,9 @@ people=s.nextInt();
                 case 'A':
                     System.out.println("check avalibility");
                     //array with the room no's will be checked
+                    for (int i = 1; i <n ; i++) {
+                        System.out.println(av[i]);
+                    }
                     break;
                 default:
                     System.out.println("Invalid character");
